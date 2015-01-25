17884f8b474c68c5c8fdd562b743131d3d20f563
Miguel Palhas <mpalhas@gmail.com>
2015-01-25 19:25:44 +0000

    Refactors tagListInput

--- gulp/watch.js
+++ gulp/watch.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 
 gulp.task('watch', ['wiredep', 'injector:css', 'injector:js'] ,function () {
   gulp.watch('src/{app,components}/**/*.scss', ['injector:css']);
+  gulp.watch('src/{app,components}/**/*.sass', ['injector:css']);
   gulp.watch('src/{app,components}/**/*.js', ['injector:js']);
   gulp.watch('src/assets/images/**/*', ['images']);
   gulp.watch('bower.json', ['wiredep']);
