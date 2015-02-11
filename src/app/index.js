95e4cd7e90c23861075df4011f97d979b3de8bab
Miguel Palhas <mpalhas@gmail.com>
2015-02-11 00:21:22 +0000

    Almost done with ng-token-auth

--- src/app/index.js
+++ src/app/index.js
@@ -8,13 +8,26 @@
       'ngSanitize',
       'ui.router',
       'angularMoment',
-      'js-data']
+      'js-data',
+      'ng-token-auth']
     )
     .config(function(Config, DSProvider) {
       DSProvider.defaults.basePath = Config.apiUrl;
     })
+    .config(function (Config, $authProvider) {
+      $authProvider.configure({
+        apiUrl: Config.apiUrl,
+        authProviderPaths: {
+          google: '/auth/google_oauth2'
+        }
+      });
+    })
     .config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
       $stateProvider
+        .state('auth', {
+          url: '/auth',
+          templateUrl: 'app/auth/index.html'
+        })
         .state('expenses', {
           url: '/expenses',
           templateUrl: 'app/expenses/index.html',
@@ -28,7 +41,7 @@
           controllerAs: 'ctrl'
         });
 
-      $urlRouterProvider.otherwise('/expenses');
+      $urlRouterProvider.otherwise('/auth');
     });
 })();
 
